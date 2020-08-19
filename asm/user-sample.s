6ed715bf90fcc39c97bf65d4be8e5a496af23af9
fluctlight <609804988@qq.com>
2020-08-19 16:34:11 +0800

    final_l2_3 59Mhz for exam 4

--- asm/user-sample.s
+++ asm/user-sample.s
@@ -9,21 +9,18 @@ __start:
     xor $v0, $v0, $v0        # v0 = 0
     xor $v1, $v1, $v1        # v1 = 0
     xor $t1, $t1, $t1        # tmp = 0
-    li $t8, 0x31111          # i_max
+    li $t8, 0x31111        # i_max
     ori $t9, $zero, 32       # j_max
     ori $a1, $zero, 1        # i = 1
 l1:
-    ori $a2, $zero, 0        # j = 0
     addu $t1, $zero, $a1     # tmp = i
 l2:
-    addiu $a2, $a2, 1        # j = j + 1
     andi $a3, $t1, 1         # tmp & 1
     addu $v0, $v0, $a3       # v0 = v0 + tmp & 1
     srl $t1, $t1, 1          # tmp >> 1
-    beq $t1, $zero, l2_end   # if tmp == 0
+    bne $t1, $zero, l2   # if tmp == 0
     xor $a3, $a3, $a3
-    bne $t9, $a2, l2         # if j != j_max
-    xor $a3, $a3, $a3   
+
     
 l2_end:
     addu $v1, $v1, $v0       # v1 = v1 + v0
